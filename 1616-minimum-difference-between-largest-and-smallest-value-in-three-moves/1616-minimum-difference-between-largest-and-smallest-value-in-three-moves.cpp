5024d745fa5c4df8c05511995976e79f5d0116a7
Deepakkr22iitk <133010709+Deepakkr22iitk@users.noreply.github.com>
2024-07-03 13:35:35 +0530

    Time: 72 ms (39.46%), Space: 38.7 MB (80.72%) - LeetHub

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -2,13 +2,15 @@ class Solution {
 public:
     int minDifference(vector<int>& nums) {
         int n=nums.size();
-        if(n<4) return 0;
+        // when n<=3 we can make each element of the vector equal.
+        if(n<4) return 0; 
         sort(nums.begin(),nums.end());
+        // in other cases where n>3 we need to first sort the vector
         int ans=INT_MAX;
-        int left=n-3;
-        for(int i=0;i<n-(left-1);i++)
+        int left=n-3-1;
+        for(int i=0;i<n-left;i++)
         {
-            ans=min(ans,nums[i+left-1]-nums[i]);
+            ans=min(ans,nums[i+left]-nums[i]);
         }
         return ans;
     }
