e681f25bc0aa746104a59fc4bacf11e29221bea1
Deepakkr22iitk <133010709+Deepakkr22iitk@users.noreply.github.com>
2024-08-20 03:30:32 +0530

    Time: 0 ms (100%), Space: 7.6 MB (28.72%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -1,7 +1,19 @@
 class Solution {
 public:
+    int countSetBits(int x)
+    {
+        int cnt=0;
+        while(x>1)
+        {
+            cnt+=(x&1);
+            x=x>>1;
+        }
+        if(x==1) cnt++;
+        return cnt;
+    }
     int minBitFlips(int start, int goal) {
         int ans=0;
+        // solution 1
         // while(start>0 || goal>0)
         // {
         //     if((start&1) != (goal&1)) ans++;
@@ -9,22 +21,26 @@ public:
         //     goal=goal>>1;
         // }
 
-        while(start>0 && goal>0)
-        {
-            if((start&1) != (goal&1)) ans++;
-            start=start>>1;
-            goal=goal>>1;
-        }
-        while(start)
-        {
-            ans+=(start&1);
-            start=start>>1;
-        }
-        while(goal)
-        {
-            ans+=(goal&1);
-            goal=goal>>1;
-        }
+        // solution 2
+        // while(start>0 && goal>0)
+        // {
+        //     if((start&1) != (goal&1)) ans++;
+        //     start=start>>1;
+        //     goal=goal>>1;
+        // }
+        // while(start)
+        // {
+        //     ans+=(start&1);
+        //     start=start>>1;
+        // }
+        // while(goal)
+        // {
+        //     ans+=(goal&1);
+        //     goal=goal>>1;
+        // }
+
+        // solution 3
+        ans=countSetBits(start^goal);
         return ans;
     }
 };
