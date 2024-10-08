e92357f33e6bbbb05edd323347701087b1b0ca87
Deepakkr22iitk <133010709+Deepakkr22iitk@users.noreply.github.com>
2024-10-08 22:23:08 +0530

    Time: 105 ms (56.18%), Space: 32.7 MB (79.33%) - LeetHub

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -2,25 +2,22 @@ class Solution {
 public:
     int minSwaps(string s) {
         int n=s.size();
-        vector<int>open;
-        for(int i=n-1;i>=0;--i)
+        int ans=0,count=0;
+        int start=0,end=n-1;
+        while(s[end]!='[') end--;
+        while(start<end)
         {
-            if(s[i]=='[') open.push_back(i);
-        }
-        int ind=0;
-        int cnt=0;
-        int ans=0;
-        for(int i=0;i<n;++i)
-        {
-            if(s[i]=='[') cnt++;
-            else cnt--;
+            if(s[start]=='[') count++;
+            else count--;
 
-            if(cnt<0)
+            if(count<0)
             {
                 ans++;
-                swap(s[i],s[open[ind++]]);
-                cnt+=2;
+                count+=2;
+                swap(s[start],s[end]);
+                while(end>start && s[end]!='[') end--;
             }
+            start++;
         }
         return ans;
     }
