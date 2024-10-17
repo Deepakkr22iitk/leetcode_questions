15091d03d04293b88fe603d9dbbca5fc328edae0
Deepakkr22iitk <133010709+Deepakkr22iitk@users.noreply.github.com>
2024-10-17 13:43:36 +0530

    Time: 0 ms (100%), Space: 7.4 MB (89.76%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maximumSwap(int num) {
+        int temp = num, base = 1, high = 1, low = 1, lowBase = 1, highBase = 1, maxDigit = 0, maxBase = 1;
+        while(temp > 0)
+        {
+            int current = temp % 10;
+            if(current<maxDigit)
+            {
+                low = maxDigit;
+                lowBase = maxBase;
+                high = current;
+                highBase = base;
+            }
+            if(current > maxDigit)
+            {
+                maxDigit = current;
+                maxBase = base;
+            }
+            base *= 10;
+            temp /= 10;
+        }
+        return num + (low-high)*(highBase-lowBase);
+    }
+};
