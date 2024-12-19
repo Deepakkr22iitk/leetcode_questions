9be9923ab06d4a7b9d5153378d80ef06149bbd31
Deepakkr22iitk <133010709+Deepakkr22iitk@users.noreply.github.com>
2024-12-19 19:17:09 +0530

    Time: 0 ms (100%), Space: 13.4 MB (13.4%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
@@ -16,12 +16,23 @@ public:
         // return arr[--idx]+k-miss;
 
         // solution 2 TC->O(log(n))
+        // here what am I trying to do?
+        // I am trying to get the number of missing numbers till that point.
+        // How can we do this? Simple taking the difference (nums[index]-index)
+        // since numbers are positive so taking 1 based indexing
+
+        // we are trying to get either the first place where number of missing numbers to be
+        // exactly equal to k or the first point where number of missing numbers 
+        // is greater than k
         if(k<=arr[0]-1) return k;
         if(k>(arr[n-1]-n)) return arr[n-1]+(k-(arr[n-1]-n));
         int low=0,high=n-1,ans=-1;
         while(low<=high)
         {
             int mid=low+(high-low)/2;
+            // if number of missing number is >= k 
+            // it can be the part of our ans but still check the left side for 
+            // the first occurence
             if((arr[mid]-(mid+1))>=k)
             {
                 ans=mid;
