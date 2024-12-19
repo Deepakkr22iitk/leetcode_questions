85b153faad810e1ea4f12d8fbda1c78547b0363e
Deepakkr22iitk <133010709+Deepakkr22iitk@users.noreply.github.com>
2024-12-19 14:56:50 +0530

    Time: 0 ms (100%), Space: 13.1 MB (34.44%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
@@ -4,14 +4,31 @@ public:
     // trying to do that
     int findKthPositive(vector<int>& arr, int k) {
         int n=arr.size();
-        int last=0,idx=0,miss=0;
         if(arr[0]>k) return k;
-        while(idx<n && (miss+(arr[idx]-last-1))<k)
+        // solution 1 TC->O(n)
+        // int last=0,idx=0,miss=0;
+        // while(idx<n && (miss+(arr[idx]-last-1))<k)
+        // {
+        //     miss+=(arr[idx]-last-1);
+        //     last=arr[idx];
+        //     idx++;
+        // }
+        // return arr[--idx]+k-miss;
+
+        // solution 2 TC->O(log(n))
+        if(k<=arr[0]-1) return k;
+        if(k>(arr[n-1]-n)) return arr[n-1]+(k-(arr[n-1]-n));
+        int low=0,high=n-1,ans=-1;
+        while(low<=high)
         {
-            miss+=(arr[idx]-last-1);
-            last=arr[idx];
-            idx++;
+            int mid=low+(high-low)/2;
+            if((arr[mid]-(mid+1))>=k)
+            {
+                ans=mid;
+                high=mid-1;
+            }
+            else low=mid+1;
         }
-        return arr[--idx]+k-miss;
+        return arr[ans]-((arr[ans]-(ans+1))-k)-1;
     }
 };
