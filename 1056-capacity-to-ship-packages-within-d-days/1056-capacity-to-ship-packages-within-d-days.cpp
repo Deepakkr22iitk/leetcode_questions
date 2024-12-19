43eaaae0ca9bcaca967a738e75c9513d6abbef3a
Deepakkr22iitk <133010709+Deepakkr22iitk@users.noreply.github.com>
2024-12-19 20:25:38 +0530

    Time: 8 ms (86.12%), Space: 35 MB (30.7%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
@@ -3,6 +3,12 @@ public:
     bool pred(vector<int> &weights,int cap,int days)
     {
         int req=0,sum=0,idx=0,n=weights.size();
+        // why are we initiating req with 0?
+        // why not with 1?
+        // because in the end when idx==n there will be one last batch which should be 
+        // counted and also if we do some handwork we will get this with ease
+
+        // so instead of copying from others without understanding spend some time on code
         while(idx<n)
         {
             if(weights[idx]>cap) return false;
@@ -13,9 +19,7 @@ public:
             }
             req++;
             sum=0;
-            // idx++;
         }
-        // req++;
         return req<=days;
     }
     int shipWithinDays(vector<int>& weights, int days) {
