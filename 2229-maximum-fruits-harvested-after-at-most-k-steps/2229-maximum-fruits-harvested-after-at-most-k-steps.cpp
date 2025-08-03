7affe3ccabeff122cf97f4ae2280f5830ff3847e
Deepakkr22iitk <133010709+Deepakkr22iitk@users.noreply.github.com>
2025-08-03 23:34:13 +0530

    Time: 386 ms (5.96%), Space: 321 MB (7.23%) - LeetHub

--- 2229-maximum-fruits-harvested-after-at-most-k-steps/2229-maximum-fruits-harvested-after-at-most-k-steps.cpp
+++ 2229-maximum-fruits-harvested-after-at-most-k-steps/2229-maximum-fruits-harvested-after-at-most-k-steps.cpp
@@ -1,41 +1,46 @@
 class Solution {
 public:
     int maxTotalFruits(vector<vector<int>>& fruits, int startPos, int k) {
-        int n = 2 * 100000 +1;
+        int n = 2*1e5+1;
 
-        vector<int> prefix(n);
+        vector<int> prefix(n); // it will stores the number of fruits till ith position
 
         for(auto it: fruits){
             prefix[it[0]] = it[1];
         }
-        for(int i = 1 ;i< prefix.size();i++)
+        for(int i = 1 ;i< n;i++)
             prefix[i] += prefix[i-1];
 
         int maxi =0;
 
-        // go to right
+        // first go to right
 
         for(int i = startPos; i <= min(startPos+k , n-1);i++){
-            int r = i;
-            int x = i-startPos;
-            int l = startPos - (k-2*x);
-            l = min(l , startPos);
-            int sum = prefix[r];
-            if(l > 0){
-                sum -= prefix[l-1];
+            int rightMost = i;
+            int diff = i-startPos;
+            int rem = (k-2*diff);
+            int leftMost = startPos;
+            if(rem>0) leftMost =  startPos - rem;  // ensuring that it is moving to the left side
+            int sum = prefix[rightMost];
+            if(leftMost > 0){
+                sum -= prefix[leftMost-1];
             }
             maxi = max(maxi , sum);
         }
 
-        //go to left
+        //first go to left
         for(int i = startPos; i >= max(startPos-k , 0);i--){
-            int l = i;
-            int x = startPos-i;
-            int r = startPos + (k-2*x);
-            r = max(min(n-1, r) , startPos);
-            int sum = prefix[r];
-            if(l > 0){
-                sum -= prefix[l-1];
+            int leftMost = i;
+            int diff = startPos-i;
+            int rem = (k - 2*diff);
+            // int rightMost = startPos + rem;
+            // rightMost = min(n-1,rightMost);
+            // rightMost = max(rightMost , startPos);
+            int rightMost=startPos;
+            if(rem>0) rightMost = min(n-1,startPos+rem); // ensuring that it doesn't cross n
+            int sum = prefix[rightMost];
+            if(leftMost > 0){
+                sum -= prefix[leftMost-1];
             }
             maxi = max(maxi , sum);
         }
