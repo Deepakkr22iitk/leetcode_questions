254009a99a9b6255137d9273e08486145b6d7fae
Deepakkr22iitk <133010709+Deepakkr22iitk@users.noreply.github.com>
2025-08-04 22:36:15 +0530

    Time: 53 ms (15.02%), Space: 74.9 MB (90.37%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -1,50 +1,23 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        int n = fruits.size();
-        int count = 0;
-        int i = 0, ans = 0;
-        int first = 0, second = -1;
-        set<int> now;
+        unordered_map<int, int> basket;
+        int start = 0, maxFruits = 0;
 
-        while (i < n) {
-            if (now.size() == 0) {
-                first = i;
-                count = 1;
-                now.insert(fruits[i]);
-            } else if (now.size() == 1) {
-                if (now.find(fruits[i]) == now.end()) {
-                    second = i;
-                    now.insert(fruits[i]);
-                    count++;
-                } else {
-                    count++;
-                    if (fruits[i] != fruits[first]) first = i;
+        for (int end = 0; end < fruits.size(); ++end) {
+            basket[fruits[end]]++;
+
+            while (basket.size() > 2) {
+                basket[fruits[start]]--;
+                if (basket[fruits[start]] == 0) {
+                    basket.erase(fruits[start]);
                 }
-            } else {
-                if (now.find(fruits[i]) == now.end()) {
-                    ans = max(ans, count);
-                    // find the last contiguous segment of one fruit type
-                    int j = i - 1;
-                    while (j > 0 && fruits[j] == fruits[j - 1]) j--;
-                    // keep that segment
-                    first = j;
-                    count = i - j;
-                    now.clear();
-                    now.insert(fruits[i]);
-                    now.insert(fruits[j]);
-                    second = i;
-                    count++;
-                } else {
-                    count++;
-                    if (fruits[i] == fruits[first]) first = i;
-                    else second = i;
-                }
+                start++;
             }
-            ans = max(ans, count);
-            i++;
+
+            maxFruits = max(maxFruits, end - start + 1);
         }
 
-        return ans;
+        return maxFruits;
     }
 };
