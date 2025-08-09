044e317d359f9073a3e68ccc30c632f1e4ffc5c0
Deepakkr22iitk <133010709+Deepakkr22iitk@users.noreply.github.com>
2025-08-10 02:56:05 +0530

    Time: 83 ms (42.92%), Space: 13.8 MB (54.25%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -8,7 +8,7 @@ public:
         for(int i=0;i<n;++i)
         {
             dp[i][i]=1;
-            if(i+1<n && s[i]==s[i+1])
+            if(i+1<n && s[i]==s[i+1])  // handling palindrome of size 2
             {
                 maxLen=2;
                 start=i;
@@ -16,34 +16,20 @@ public:
             } 
         }
 
-        for(int len=3;len<=n;++len)
+        for(int len=3;len<=n;++len) // considering length 'len' of palindrome starting at index 'i'
         {
             for(int i=0;i<n-len+1;++i)
             {
-                int j=i+len-1;
+                int j=i+len-1; 
                 if(dp[i+1][j-1]==1 && s[i]==s[j])
                 {
                     dp[i][j]=1;
-                    if(len>maxLen)
-                    {
-                        start=i;
-                        maxLen=len;
-                    }
+                    start=i;
+                    maxLen=len;
                 }
 
             }
         }
-
-        // for(int i=0;i<n;++i)
-        // {
-        //     for(int j=0;j<n;++j)
-        //     {
-        //         cout<<dp[i][j]<<" ";
-        //     }
-        //     cout<<"\n";
-        // }
-
-        // cout<< start<<" "<< maxLen<<" ";
 
         return s.substr(start,maxLen);
 
