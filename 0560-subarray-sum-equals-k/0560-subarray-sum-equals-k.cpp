04834e7456fa01fabc40fd7eded270cf118a62c1
Deepakkr22iitk <133010709+Deepakkr22iitk@users.noreply.github.com>
2025-08-16 12:44:46 +0530

    Time: 67 ms (37.92%), Space: 47.8 MB (28.14%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_set<int> sumSet;
         map<int,int> freq;
         // Traverse through array
         // and store prefix sums
         int sum = 0,count=0;
         freq[sum]++;
-        sumSet.insert(sum);
         for (int i = 0; i < nums.size(); i++) {
             sum += nums[i];
 
             // If prefix sum is 0 or
             // it is already present
-            if (sumSet.find(sum-k) != sumSet.end())
+            if (freq.find(sum-k) != freq.end())
                 count+=freq[sum-k];
 
-            sumSet.insert(sum);
             freq[sum]++;
         }
         return count;
