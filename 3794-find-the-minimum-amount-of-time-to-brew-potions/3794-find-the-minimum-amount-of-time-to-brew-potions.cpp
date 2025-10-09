4c8356be00645fe4cc7703d591fea880a03a697d
Deepakkr22iitk <133010709+Deepakkr22iitk@users.noreply.github.com>
2025-10-09 20:55:34 +0530

    Time: 351 ms (71.71%), Space: 45.6 MB (43.43%) - LeetHub

--- 3794-find-the-minimum-amount-of-time-to-brew-potions/3794-find-the-minimum-amount-of-time-to-brew-potions.cpp
+++ 3794-find-the-minimum-amount-of-time-to-brew-potions/3794-find-the-minimum-amount-of-time-to-brew-potions.cpp
@@ -4,35 +4,31 @@ public:
     long long minTime(vector<int>& skill, vector<int>& mana) {
         int n=skill.size(),m=mana.size();
 
-        vector<long long> time(n,0);
-        long long t=0;
-        for(int i=0;i<n;++i)
+        vector<long long> time(n,0); // we will update it sequentially for every mana
+        long long tillNow=0;
+        for(int i=0;i<n;++i) // we are calculating for first mana only here
         {
-            int value=mana[0];
-            int kaam=skill[i];
-            long long v=1ll*value;
-            long long k=1ll*kaam;
-            t+=v*k;
-            time[i]=t;
+            tillNow+=(1ll*mana[0]*skill[i]);
+            time[i]=tillNow;
         }
 
-        vector<long long> prevTime(n,0);
+        vector<long long> beforeHand(n,0); // we are trying to get the skills of persons before
         int sum=0;
         for(int i=0;i<n;++i)
         {
-            prevTime[i]=sum;
+            beforeHand[i]=sum;
             sum+=(1ll*skill[i]);
         }
 
-        for(int i=1;i<m;++i)
+        for(int i=1;i<m;++i) // we have calculate for 1st previously
         {
-            long long minTime=time[0];
-            for(int j=n-1;j>0;--j)
+            long long minTime=time[0]; // least time for the next which is possible before calculating
+            for(int j=n-1;j>0;--j) // in this loop we are trying to get the minTime to start next mana
             {
-                minTime=max(minTime,time[j]-(prevTime[j]*(1ll*mana[i])));
+                minTime=max(minTime,time[j]-(beforeHand[j]*(1ll*mana[i])));
             }
             
-            for(int k=0;k<n;++k)
+            for(int k=0;k<n;++k) // updating the time of completing mana[i] by skill[k]
             {
                 minTime+=(1ll*skill[k]*mana[i]);
                 time[k]=minTime;
