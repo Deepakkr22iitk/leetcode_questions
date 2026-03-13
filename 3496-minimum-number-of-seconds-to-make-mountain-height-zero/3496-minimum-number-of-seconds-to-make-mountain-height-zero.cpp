d6f984da618dc2559ea08e018271c358aeffce4b
Deepakkr22iitk <133010709+Deepakkr22iitk@users.noreply.github.com>
2026-03-13 20:54:06 +0530

    Time: 13 ms (75.48%), Space: 29 MB (43.87%) - LeetHub

--- 3496-minimum-number-of-seconds-to-make-mountain-height-zero/3496-minimum-number-of-seconds-to-make-mountain-height-zero.cpp
+++ 3496-minimum-number-of-seconds-to-make-mountain-height-zero/3496-minimum-number-of-seconds-to-make-mountain-height-zero.cpp
@@ -1,55 +1,22 @@
 class Solution {
 public:
-    bool limit(long long mid,long long mountainHeight,auto &workerTimes)
-    {
-        long long temp=0;
-        long long len=workerTimes.size();
-        for(long long i=0;i<len;++i)
-        {
-            long long curr=0;
-            long long cost=1LL*workerTimes[i];
-            long long l=1,h=mountainHeight;
-            
-            while(l<=h)
-            {
-                long long m=l+(h-l)/2;
-                long long curr_cost=(m*(m+1))/2 *cost;
-                
-                if(curr_cost<=mid)
-                {
-                    l=m+1;
-                }
-                else h=m-1;
-            }
-            
-            temp=temp+h;
-            if(temp>=mountainHeight) return true;
-            
-            // for(long long j=1;j<=mountainHeight;++j)
-            // {
-            //     if((curr+j*cost)<=mid) 
-            //     {
-            //         temp++;
-            //         curr=curr+j*cost;
-            //     }
-            // }
-        }
-        bool ans=(temp>=mountainHeight);
-        return ans;
+    inline static long long f(long long t, vector<int>& workerTimes){
+        long long x=0;
+        for(int w: workerTimes)
+            x+=(long long)((sqrt(1+8.0*t/w)-1)/2);
+        return x;
     }
-    long long minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
-        long long l=0,h=1e18;
-        long long dk=0;
-        while(l<=h)
-        {
-            long long mid=l+(h-l)/2;
-            if(limit(mid,1LL*mountainHeight,workerTimes))
-            {
-                dk=mid;
-                h=mid-1;
-            }
-            else l=mid+1;
+    static long long minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
+        int wz=workerTimes.size();
+        if (wz==1)
+            return (long long)workerTimes[0]*mountainHeight*(mountainHeight+1)/2;
+        long long l=1, r=1e12*(long long)mountainHeight/wz;
+        while(l<r){
+            long long m=l+(r-l)/2;
+            long long x=f(m, workerTimes);
+            if(x>=mountainHeight) r=m;
+            else l=m+1;
         }
-        return dk;
+        return l;
     }
 };
