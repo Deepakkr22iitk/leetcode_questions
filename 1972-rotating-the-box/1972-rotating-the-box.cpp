c58b900ed04731a861207704f864df925e974be7
Deepakkr22iitk <133010709+Deepakkr22iitk@users.noreply.github.com>
2026-05-06 23:43:11 +0530

    Time: 11 ms (65.85%), Space: 56.7 MB (90.17%) - LeetHub

--- 1972-rotating-the-box/1972-rotating-the-box.cpp
+++ 1972-rotating-the-box/1972-rotating-the-box.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
-    vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
-        int n=box.size();
-        int m=box[0].size();
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& boxGrid) {
+        int m = boxGrid.size();
+        int n = boxGrid[0].size();
 
-        vector<vector<char>>res(m,vector<char>(n));
-        for(int i=0;i<n;++i)
-        {
-            int empty=m-1;
-            for(int j=m-1;j>=0;--j)
-            {
-                if(box[i][j]=='*') empty=j-1; //If the current cell contains an obstacle. so, the next available position is (j-1)
+        // Step 1: simulate gravity (right side)
+        for (int i = 0; i < m; i++) {
+            int empty = n - 1;
 
-                if(box[i][j]=='#')
-                {
-                    box[i][j]='.';
-                    box[i][empty]='#';
-                    --empty;
+            for (int j = n - 1; j >= 0; j--) {
+                if (boxGrid[i][j] == '*') {
+                    empty = j - 1;
+                } 
+                else if (boxGrid[i][j] == '#') {
+                    swap(boxGrid[i][j], boxGrid[i][empty]);
+                    empty--;
                 }
-            }    
-        }
-
-        for(int i=0;i<n;++i)
-        {
-            for(int j=m-1;j>=0;--j)
-            {
-                res[j][n-i-1]=box[i][j];
             }
         }
+
+        // Step 2: rotate matrix
+        vector<vector<char>> res(n, vector<char>(m));
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                res[j][m - 1 - i] = boxGrid[i][j];
+            }
+        }
+
         return res;
     }
 };
