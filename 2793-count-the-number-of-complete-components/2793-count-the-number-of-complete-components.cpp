da0277686314dd4c68fada41e654272afd2e21e8
Deepakkr22iitk <133010709+Deepakkr22iitk@users.noreply.github.com>
2026-07-11 22:53:26 +0530

    Time: 35 ms (78.43%), Space: 136.6 MB (37.36%) - LeetHub

--- 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.cpp
@@ -1,66 +1,47 @@
 class Solution {
 public:
-    void dfs(vector<int>adj[],int &n,int st,vector<int>&vis,int& nodes,int p)
-    {
-        vis[st]=1;
-        nodes++;
-        
-        for(auto x:adj[st])
-        {
-            if(x!=p)
-            {
-                if(vis[x]==0)
-                {
-                    dfs(adj,n,x,vis,nodes,st);
-                }
-            }
-        }
-    }
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        vector<int>adj[n];
-        int m=edges.size();
-        vector<vector<int>>rk(n,vector<int>(n));
-        for(int i=0;i<m;++i)
-        {
-            int u=edges[i][0];
-            int v=edges[i][1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-            rk[u][v]=1;
-            rk[v][u]=1;
+        vector<vector<int>> adj(n);
+
+        for (auto &e : edges) {
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
         }
-        
-        int ans=0;
-        
-        vector<int>vis(n);      
-        for(int i=0;i<n;++i)          
-        {
-            if(vis[i]==0)
-            {
-                int nodes=0;
-                vector<int>visit(n);    
-                dfs(adj,n,i,visit,nodes,-1);
-                int val=0;
-                for(int l=0;l<n;++l)
-                {
-                    if(visit[l]==1)
-                    {
-                       vis[l]=1;
-                       for(int j=0;j<n;++j)
-                       {
-                           if(visit[j]==1)
-                           {
-                                if(l==j) continue;
-                                if(rk[l][j]==1) val++;
-                           }
-                       }   
+
+        vector<bool> vis(n, false);
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (vis[i]) continue;
+
+            queue<int> q;
+            q.push(i);
+            vis[i] = true;
+
+            int nodes = 0;//number of nodes in the component
+            int edgeCount = 0; //total edges in the component
+
+            while (!q.empty()) {
+                int u = q.front();
+                q.pop();
+
+                nodes++;
+                edgeCount += adj[u].size();
+
+                for (int v : adj[u]) {
+                    if (!vis[v]) {
+                        vis[v] = true;
+                        q.push(v);
                     }
                 }
-                val/=2;
-                int rk=nodes*(nodes-1)/2;
-                if(val==rk) ans++;
             }
+
+            edgeCount /= 2;
+
+            if (edgeCount == nodes * (nodes - 1) / 2)
+                ans++;
         }
+
         return ans;
     }
 };
