55668722ba99997c6b1fa966a5ccea1bbe2cf9c6
Deepakkr22iitk <133010709+Deepakkr22iitk@users.noreply.github.com>
2026-08-02 20:41:08 +0530

    Time: 14 ms (21.59%), Space: 19.8 MB (11.06%) - LeetHub

--- 0909-stone-game/0909-stone-game.cpp
+++ 0909-stone-game/0909-stone-game.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
+    int solve(int i, int j, vector<int>& piles, vector<vector<int>>& dp, vector<int>& pref) {
+        if (i > j) return 0;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        int total = pref[j + 1] - pref[i];
+
+        int left = solve(i + 1, j, piles, dp, pref);
+        int right = solve(i, j - 1, piles, dp, pref);
+
+        return dp[i][j] = total - min(left, right);
+    }
+
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int n = piles.size();
+        vector<int> pref(n + 1, 0);
+
+        for (int i = 0; i < n; i++)
+            pref[i + 1] = pref[i] + piles[i];
+
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+
+        int total = pref[n];
+
+        int first = solve(0, n - 1, piles, dp, pref);
+
+        return first >= total - first;
     }
 };
