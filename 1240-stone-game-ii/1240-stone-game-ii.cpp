ce526b85292dfef5a3666ab6b560c1b9fb8ce802
Deepakkr22iitk <133010709+Deepakkr22iitk@users.noreply.github.com>
2026-08-09 13:11:24 +0530

    Time: 11 ms (54.67%), Space: 14.2 MB (33.69%) - LeetHub

--- 1240-stone-game-ii/1240-stone-game-ii.cpp
+++ 1240-stone-game-ii/1240-stone-game-ii.cpp
@@ -1,27 +1,30 @@
 class Solution {
+private: 
+    vector<vector<int>> memo; 
+    int getScore(vector<int> &piles, int pos, int m) {
+        if(pos == piles.size()) return 0; // noscore 
+        if(memo[pos][m] != -1) return memo[pos][m]; 
+        int cur = 0; 
+        int best = INT_MIN; 
+        for(int i = pos; i < piles.size() && i < pos + 2 * m; i++) {
+            cur += piles[i]; 
+            best = max(best, cur - getScore(piles, i + 1, max(m, i - pos + 1))); 
+        }
+
+        return memo[pos][m] = best; 
+    }
 public:
     int stoneGameII(vector<int>& piles) {
-        int n=piles.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1));
-        vector<int>suf_sm(n+1);
-        for(int i=n-1;i>=0;--i)
-        {
-            suf_sm[i]=suf_sm[i+1]+piles[i];
-        }
-        for(int i=0;i<=n;++i)
-        {
-            dp[i][n]=suf_sm[i];
-        }
-        for(int i=n-1;i>=0;--i)
-        {
-            for(int j=n-1;j>=1;--j)
-            {
-                for(int X=1;X<=2*j && i+X<=n;++X)
-                {
-                    dp[i][j]=max(dp[i][j],suf_sm[i]-dp[i+X][max(j,X)]);
-                }
-            }
-        }
-        return dp[0][1];  
+        // find the net optimal difference 
+        // a + b = piles.sum 
+        // a- b = nedDiff 
+        // a = (piles.sum + netDiff) / 2 
+        memo = vector<vector<int>>(piles.size(), vector<int>(100, -1)); 
+    
+        int netDiff = getScore(piles, 0, 1); 
+        int sum = 0; 
+        for(int x: piles) sum+= x; 
+        int a = (netDiff + sum) / 2; 
+        return a;
     }
 };
