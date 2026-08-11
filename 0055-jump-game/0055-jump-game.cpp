15b3bf1b3224df23128315cb17a673bac3f7109e
Deepakkr22iitk <133010709+Deepakkr22iitk@users.noreply.github.com>
2026-08-11 21:26:52 +0530

    Time: 0 ms (100%), Space: 52.2 MB (83.73%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        int ind=0,maxi=nums[0];
+        while(ind<n && ind<=maxi)
+        {
+            maxi=max(maxi,ind+nums[ind]);
+            ind++;
+        }
+
+        return ind==n;
+    }
+};
