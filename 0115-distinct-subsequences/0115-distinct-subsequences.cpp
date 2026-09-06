f072ad2863dd416a7001dbfe29119955dc55368d
Deepakkr22iitk <133010709+Deepakkr22iitk@users.noreply.github.com>
2026-09-06 15:11:59 +0530

    Time: 31 ms (53.36%), Space: 27 MB (52.57%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    // int f_recurr(int i, int j, string &s, string &t){
+    //     if(j < 0) return 1;
+    //     if(i < 0) return 0;
+
+    //     if(s[i] == t[j]) return f_recurr(i - 1, j, s, t) + f_recurr(i - 1, j - 1, s, t);
+
+    //     else return f_recurr(i - 1, j, s, t);
+    // }
+
+    int f_memo(int i, int j, string &s, string &t, vector<vector<int>> &dp){
+        if(j == 0) return 1;
+        if(i == 0) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+        if(s[i - 1] == t[j - 1]) return dp[i][j]=(f_memo(i - 1, j, s, t, dp) + f_memo(i - 1, j - 1, s, t, dp));
+
+        else return dp[i][j]=f_memo(i - 1, j, s, t, dp);
+    }
+    int numDistinct(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+
+        // return f_recurr(n-1,m-1,s,t);
+
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+        return f_memo(n, m, s, t, dp);
+
+        // vector<vector<long long>> dp(n + 1, vector<long long>(m + 1, 0));
+        // for(int i = 0; i <= m; i++) dp[i][0] = 1;
+
+        // for(int i = 1; i <= n; i++){
+        //     for(int j = 1; j <= m; j++){
+        //         if(s[i - 1] == t[j - 1]) dp[i][j] = dp[i - 1][j] 
+        //             + dp[i - 1][j - 1];
+
+        //         else dp[i][j] = dp[i - 1][j];
+        //     }
+        // }
+
+        // return dp[n][m];
+
+        // vector<long long> prev(m + 1, 0), curr(m + 1, 0);
+        // prev[0] = curr[0] = 1;
+
+        // for(int i = 1; i <= n; i++){
+        //     for(int j = 1; j <= m; j++){
+        //         if(s[i - 1] == t[j - 1]) curr[j] = prev[j] 
+        //             + prev[j - 1];
+
+        //         else curr[j] = prev[j];
+        //     }
+        //     prev = curr;
+        // }
+
+        // return prev[m];
+
+    }
+};
